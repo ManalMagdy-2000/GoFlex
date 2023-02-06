51caf35b7ebd737d1182dfd3a21652788e02cecc
Manal <manalmagdy694@gmail.com>
2023-02-06 18:55:30 +0800

    navbar created , 30% done

--- flexISAngular/src/app/sidenav/sidenav.module.ts
+++ flexISAngular/src/app/sidenav/sidenav.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SidenavRoutingModule } from './sidenav-routing.module';
-import { SidenavComponent } from './sidenav.component';
 
 
 @NgModule({
   declarations: [
-    SidenavComponent
+
   ],
   imports: [
     CommonModule,
