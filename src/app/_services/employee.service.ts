31ae003cdefdf9eb9e62c326353f0587ad7a270e
Manal <manalmagdy694@gmail.com>
2023-02-26 23:54:10 +0800

    UI for login Page , side nav updated

---
+++ src/app/_services/employee.service.ts
@@ -0,0 +1,51 @@
+
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from '@environments/environment';
+import { Request, Employee, Offer } from '@app/_models';
+import { Supervisor } from '../_models/supervisor';
+
+@Injectable({ providedIn: 'root' })
+export class EmployeeService {
+
+    constructor(
+        private router: Router,
+        private http: HttpClient
+    ) { }
+
+    addEmployee(employee: Employee) {
+        return this.http.post(`${environment.apiUrl}/employees/add`, employee);
+    }
+
+    getAllEmployees() {
+        return this.http.get<Employee[]>(`${environment.apiUrl}/employees`);
+    }
+
+    getEmployeeById(id: string) {
+        return this.http.get<Employee>(`${environment.apiUrl}/employees/${id}`);
+    }
+
+    addRequest(id: string, request: Request) {
+        return this.http.post(`${environment.apiUrl}/departments/request/${id}`, request);
+    }
+
+    getRequestById(employeeID: string, requestID: string) {
+        return this.http.get<Request>(`${environment.apiUrl}/employees/${employeeID}/request/${requestID}`);
+    }
+
+    addSupervisor(supervisorID: string, Supervisor: Supervisor) { //add supervisor
+        return this.http.post(`${environment.apiUrl}/employees/Admin/${supervisorID}`, { Supervisor });
+    }
+
+    addOffer(employeeID: string, requestID: string, offer: Offer) {
+        return this.http.post(`${environment.apiUrl}/employees/${employeeID}/request/${requestID}/offer`, offer);
+    }
+
+    updateStatus(employeeID: string, requestID: string, offerID: string, status: string) {
+        console.log(employeeID, requestID, offerID, status);
+        return this.http.post(`${environment.apiUrl}/employees/${employeeID}/request/${requestID}/offer/${offerID}`, { status });
+    }
+}
---
+++ src/app/users/list.component.ts
@@ -0,0 +1,26 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { AccountService } from '@app/_services';
+import { Employee } from '../_models/employee';
+
+@Component({ templateUrl: 'list.component.html' })
+export class ListComponent implements OnInit {
+    employees = null;
+    employee: Employee[] = [];
+    constructor(private accountService: AccountService) {}
+
+    ngOnInit() {
+        this.accountService.getAll()
+            .pipe(first())
+            .subscribe(employees => this.employees = employees);
+    }
+
+    deleteEmployee(id: string) {
+        const employee = this.employees.find(x => x.id === id);
+        employee.isDeleting = true;
+        this.accountService.delete(id)
+            .pipe(first())
+            .subscribe(() => this.employees = this.employees.filter(x => x.id !== id));
+    }
+}
